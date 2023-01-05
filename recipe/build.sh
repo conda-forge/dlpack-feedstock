11f7b17f61971ba59ac369a4faa31e41ef133f33
Keith Kraus <keith.j.kraus@gmail.com>
2023-01-05 17:54:18 -0500

    Do cmake install instead of manually copying

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,10 @@
-cmake ${CMAKE_ARGS} 	-DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_INSTALL_PREFIX=$PREFIX \
-	-DCMAKE_PREFIX_PATH=$PREFIX \
-	-DBUILD_DOCS=OFF \
-	-DCMAKE_INSTALL_LIBDIR=$PREFIX/lib
+cmake ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DBUILD_DOCS=OFF \
+  -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib
 
-make -j$CPU_COUNT
-
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/include/dlpack
-cp include/dlpack/*.h $PREFIX/include/dlpack/.
-cp bin/mock $PREFIX/bin/.
-
-
+cmake --build . --parallel $CPU_COUNT
+cmake --install .
 
